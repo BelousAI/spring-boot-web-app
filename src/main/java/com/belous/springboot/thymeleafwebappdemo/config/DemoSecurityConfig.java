678c98934362180b188c41f4898588282c150621
BelousAnton <anton.belous@customertimes.com>
2022-01-16 20:22:04 +0300

    Display content based on user role

--- src/main/java/com/belous/springboot/thymeleafwebappdemo/config/DemoSecurityConfig.java
+++ src/main/java/com/belous/springboot/thymeleafwebappdemo/config/DemoSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -23,5 +24,26 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // use jdbc authentication
         auth.jdbcAuthentication().dataSource(securityDataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/employees/showForm*").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/employees/save").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/employees/delete").hasRole("ADMIN")
+                .antMatchers("/employees/**").hasRole("EMPLOYEE")
+                .antMatchers("/resources/**").permitAll()
+                .and()
+                .formLogin()
+                    .loginPage("/showMyLoginPage")
+                    .loginProcessingUrl("/authenticateTheUser")
+                    .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/access-denied");
     }
 }
